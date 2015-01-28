963cef72a4bc9b57c501157219a8c5e5cb74600f
Jacqueline <Jacqueline@Jacquelines-MacBook-Pro.local>
2015-01-27 20:05:42 -1000

    swapTwoWords (regular and reg ex version) done

--- dsma.js
+++ dsma.js
@@ -12,4 +12,26 @@ findMissingNums = function (stringToCheck) {
   return missingNums;
 };
 
-var missingNums = findMissingNums("9899100101103104105");
+var missingNums = findMissingNums("9899100101103104105"); 
+
+swapTwoWords = function (twoWords) {
+  var words = twoWords.split(" ");
+
+  //concatenate second word with a comma and
+  //first word
+  return words[1] + ", " + words[0];
+};
+
+/*
+  Let's do this the REG EX way:
+  http://stackoverflow.com/questions/8688878/extracting-words-in-sentence-with-regex?rq=1
+*/
+swapTwoWords2 = function (twoWords) {
+  twoWords.replace(/^(.*)\s+(\w+)$/, "$2, $1");
+};
+
+/*
+rollyString = function (stringA, stringB) {
+
+};
+*/
